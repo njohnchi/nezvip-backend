a1e79728026721fc7a44f3613ca0e7860375f8bd
Johnchi <njohnchi@gmail.com>
2026-03-08 22:00:06 +0100

    update home route to redirect based on authentication status

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Laravel\Fortify\Features;
 
-Route::inertia('/', 'Welcome', [
-    'canRegister' => Features::enabled(Features::registration()),
-])->name('home');
+Route::get('/', function () {
+    return redirect(auth()->check() ? '/dashboard' : '/login');
+})->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::inertia('dashboard', 'Dashboard')->name('dashboard');
